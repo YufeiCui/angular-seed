15072e5b83ffacee6837cc29ded1edae2e7e4057
Yufei Cui <yufei.cui@ibm.com>
2018-05-15 11:08:24 -0400

    Minor Styling

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -6,7 +6,8 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl',
+    css: 'view1/view1.css'
   });
 }])
 
